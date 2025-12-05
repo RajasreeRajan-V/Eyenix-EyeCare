9e2531f68b5ba76d0fc7b71a19ec57631e5c22dd
RajasreeRajan-V <rajasreevjob00@gmail.com>
2025-12-05 17:06:58 +0530

    user-dashboad

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/' , [CustomerController::class, 'dashboard'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
